2145ea7dc2bddd2c6f078ed270fb3a60bb72fdd6
Arun Patwardhan <arun@amaranthine.co.in>
2020-03-26 15:58:44 +0530

    Corrected log file name.

--- DATA/DISK IMAGES/configureDiskImages.sh
+++ DATA/DISK IMAGES/configureDiskImages.sh
@@ -71,7 +71,7 @@ then
     echo "$(date "+DATE: %Y-%m-%d%TIME: %H:%M:%S") File exists" >> $LOGFILE
 else
     cd /Users/Shared/
-    touch configureContentCaching.log
+    touch configureDiskImages.log
     echo "$(date "+DATE: %Y-%m-%d%TIME: %H:%M:%S") Created Folder" >> $LOGFILE
 fi
 

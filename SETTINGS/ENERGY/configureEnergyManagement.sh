0534cc06f98cb63748b554131177669c5c4f0641
Arun Patwardhan <arun@amaranthine.co.in>
2020-03-26 18:01:36 +0530

    Update the script description.

--- SETTINGS/ENERGY/configureEnergyManagement.sh
+++ SETTINGS/ENERGY/configureEnergyManagement.sh
@@ -4,7 +4,7 @@
 #Date    : 26th March 2020
 #Contact : arun@amaranthine.co.in
 #Website : https://www.amaranthine.in
-#Scope   : This script configures the Disk images.
+#Scope   : This script configures the Energy Preferences.
 #Arguments
 
 #NOTE       : This script must be run as sudo.
